0180b2f90893d50ab5e699efad3a295a1d982161
Wendell <105543343+XiaoWendell@users.noreply.github.com>
2024-11-25 20:53:46 +0800

    更新本地日期时间工具的注释

--- _javascript/modules/components/locale-datetime.js
+++ _javascript/modules/components/locale-datetime.js
@@ -1,10 +1,10 @@
 /**
- * Update month/day to locale datetime
+ * 将月份/日期更新为本地日期时间
  *
- * Requirement: <https://github.com/iamkun/dayjs>
+ * 要求：<https://github.com/iamkun/dayjs>
  */
 
-/* A tool for locale datetime */
+/* 本地日期时间工具 */
 class LocaleHelper {
   static get attrTimestamp() {
     return 'data-ts';
@@ -19,7 +19,7 @@ class LocaleHelper {
   }
 
   static getTimestamp(elem) {
-    return Number(elem.getAttribute(this.attrTimestamp)); // unix timestamp
+    return Number(elem.getAttribute(this.attrTimestamp)); // unix 时间戳
   }
 
   static getDateFormat(elem) {
@@ -40,12 +40,12 @@ export function initLocaleDatetime() {
       elem.removeAttribute(LocaleHelper.attrTimestamp);
       elem.removeAttribute(LocaleHelper.attrDateFormat);
 
-      // setup tooltips
+      // 设置工具提示
       if (
         elem.hasAttribute('data-bs-toggle') &&
         elem.getAttribute('data-bs-toggle') === 'tooltip'
       ) {
-        // see: https://day.js.org/docs/en/display/format#list-of-localized-formats
+        // 参见：https://day.js.org/docs/en/display/format#list-of-localized-formats
         const tooltipText = date.format('llll');
         elem.setAttribute('data-bs-title', tooltipText);
       }
